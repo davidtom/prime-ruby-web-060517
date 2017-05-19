2e0425d678763b53f8b375832244520f2053b66d
davidtom <davidjtomczyk@gmail.com>
2017-05-19 14:57:38 -0400

    implemented prime? method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    false
+  else
+    #Get divisors up to sqrt(n) (all others will be repeats)
+    divisors = (2..Math.sqrt(n))
+
+    #Go through divisors and see if any can divide fully (ie no remainder)
+    divisors.each do |divisor|
+      return false if n % divisor == 0
+    end
+    #If no divisor can fully divide, number must be prime: return true
+    return true
+  end
+end
